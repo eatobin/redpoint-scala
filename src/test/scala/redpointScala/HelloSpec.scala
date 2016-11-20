15aff6c42c76295d8b9eabfc7e4a9f1cf9c2d56a
Eric Tobin <eatobin@gmail.com>
2016-11-20 11:51:53 -0600

    prepare

--- src/test/scala/redpointScala/HelloSpec.scala
+++ src/test/scala/redpointScala/HelloSpec.scala
@@ -1,7 +1,9 @@
+package redpointScala
+
 import org.scalatest._
 
 class HelloSpec extends FlatSpec with Matchers {
   "Hello" should "have tests" in {
-    true should === (true)
+    true should ===(true)
   }
 }
