164207f1c2d70899f399ff9f4a9acd06179aab38
Eric Tobin <eatobin@gmail.com>
2016-11-22 16:32:00 -0600

    progress

--- src/main/scala/RosterUtility.scala
+++ src/main/scala/RosterUtility.scala
@@ -1,6 +1,6 @@
 case class GiftPair(givee: Symbol, giver: Symbol)
 
-case class Player(pName: String, giftHist: List[GiftPair])
+case class Player(pName: String, giftHist: Vector[GiftPair])
 
 object RosterUtility {
 
@@ -15,21 +15,16 @@ object RosterUtility {
   def makePlayersList(rosterList: List[List[String]]): List[List[String]] =
     rosterList.tail
 
-  def f(ab: (Int, Int)): Int =
-    ab match {
-      case (a, b) => a + b
-    }
-
-  def makePlayerKV(kv: (String, String, String, String)): (String, Player) =
+  def makePlayerKV(kv: List[String]): (Symbol, Player) =
     kv match {
-      case (s, pn, ge, gr) =>
+      case List(s, pn, ge, gr) =>
         val gp = GiftPair(Symbol(ge), Symbol(gr))
-        val plr = Player(pn, List(gp))
-        (s, plr)
+        val plr = Player(pn, Vector(gp))
+        (Symbol(s), plr)
     }
 
-  def makePlayersKVList(playersList: List[List[String]]) =
-    playersList.productIterator.map { case (k, v) => k.toString -> Player }
+  //  def makePlayersKVList(playersList: List[List[String]]) =
+  //    playersList.productIterator.map { case (k, v) => k.toString -> Player }
 
   //  def makePlayerMap(kv: (String, String, String, String)): Map[Symbol, Player] =
   //    kv match {
@@ -46,4 +41,8 @@ object RosterUtility {
 
 // :load /Users/eatobin/scala_projects/redpoint-scala/src/main/scala/RosterUtility.scala
 // :load /home/eric/scala_projects/redpoint-scala/src/main/scala/RosterUtility.scala
-// testRosterList = [["The Beatles","2014"],["RinSta","Ringo Starr","JohLen","GeoHar"],["JohLen","John Lennon","PauMcc","RinSta"],["GeoHar","George Harrison","RinSta","PauMcc"],["PauMcc","Paul McCartney","GeoHar","JohLen"]]
+// val bs = "The Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen"
+// val rl = RosterUtility.makeRosterList(bs)
+// val pl = RosterUtility.makePlayersList(rl)
+// val hd = pl.head
+// val kv = RosterUtility.makePlayerKV(hd)
