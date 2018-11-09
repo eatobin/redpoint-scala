b198f01a23d0a0941ba8b94f985851adc5e3e914
Eric Tobin <eatobin@gmail.com>
2018-11-09 15:29:55 -0600

    progress

--- src/main/scala/redpoint/RosterStringCheck.scala
+++ src/main/scala/redpoint/RosterStringCheck.scala
@@ -2,15 +2,27 @@ package redpoint
 
 object RosterStringCheck {
 
+  // Remove the spaces between CSVs and any final \n
   def scrub(rawString: RawString): Scrubbed = rawString.stripLineEnd.replaceAll(", ", ",")
 
+  // Split string into lines
   def lines(scrubbed: Scrubbed): Array[String] = scrubbed.split('\n')
 
+  // Ensure string is not nil, empty or only spaces. Returns a scrubbed string
   def nonBlankString(rawString: RawString): Either[ErrorString, ResultString] = {
     if (rawString == null || rawString.trim.isEmpty) {
-      Left("The roster string was nil, empty or only spaces")
+      Left("the roster string was nil, empty or only spaces")
     } else {
       Right(scrub(rawString))
+    }
+  }
+
+  // A string of newlines >= 4?
+  def validLengthString(scrubbed: Scrubbed): Either[ErrorString, ResultString] = {
+    if (scrubbed.filter(_ == '\n').length < 4) {
+      Left("roster string is not long enough")
+    } else {
+      Right(scrubbed)
     }
   }
 }
