61482600057bca5633e19d29767ed0453928d86c
Eric Tobin <eatobin@gmail.com>
2018-11-13 20:57:17 -0600

    testing progress

--- src/main/scala/redpoint/package.scala
+++ src/main/scala/redpoint/package.scala
@@ -1,28 +1,28 @@
 package object redpoint {
 
-  //  type RawString = String
-  //  type Scrubbed = String
-  //  type ErrorString = String
-
-  type RosterString = String
-  type RosterList = List[List[String]]
-  type InfoLine = List[String]
-  type PlayersList = List[List[String]]
-  type PlayerLine = List[String]
-
-  type PlrSym = Symbol
-  type Givee = PlrSym
-  type Giver = PlrSym
-
-  type PName = String
-  type GiftHist = Vector[GiftPair]
-  type GYear = Int
+  type RawString = String
+  type Scrubbed = String
+  type ErrorString = String
 
   type RName = String
   type RYear = Int
+  //    type RosterString = String
+  //  type RosterList = List[List[String]]
+  //  type InfoLine = List[String]
+  //  type PlayersList = List[List[String]]
+  //  type PlayerLine = List[String]
+  //
+  //  type PlrSym = Symbol
+  //  type Givee = PlrSym
+  //  type Giver = PlrSym
+  //
+  //  type PName = String
+  //  type GiftHist = Vector[GiftPair]
+  //  type GYear = Int
 
-  type PlayerKV = (PlrSym, Player)
-  type PlayersKVList = List[(PlrSym, Player)]
-  type PlayersMap = Map[PlrSym, Player]
+  //
+  //  type PlayerKV = (PlrSym, Player)
+  //  type PlayersKVList = List[(PlrSym, Player)]
+  //  type PlayersMap = Map[PlrSym, Player]
 
 }
--- src/test/scala/redpoint/RosterSpec.scala
+++ src/test/scala/redpoint/RosterSpec.scala
@@ -1,42 +1,48 @@
 package redpoint
 
 import org.scalatest.FlatSpec
+import redpoint.Roster._
 
 class RosterSpec extends FlatSpec {
 
   val bs = "The Beatles, 2014\nRinSta, Ringo Starr, JohLen, GeoHar\nJohLen, John Lennon, PauMcc, RinSta\nGeoHar, George Harrison, RinSta, PauMcc\nPauMcc, Paul McCartney, GeoHar, JohLen\n"
-  val rl = List(List("The Beatles", "2014"), List("RinSta", "Ringo Starr", "JohLen", "GeoHar"), List("JohLen", "John Lennon", "PauMcc", "RinSta"), List("GeoHar", "George Harrison", "RinSta", "PauMcc"), List("PauMcc", "Paul McCartney", "GeoHar", "JohLen"))
-  val badRL = List(List(""))
-  val plist = List(List("RinSta", "Ringo Starr", "JohLen", "GeoHar"), List("JohLen", "John Lennon", "PauMcc", "RinSta"), List("GeoHar", "George Harrison", "RinSta", "PauMcc"), List("PauMcc", "Paul McCartney", "GeoHar", "JohLen"))
-  val pmap: redpoint.PlayersMap = Map('RinSta -> Player("Ringo Starr", Vector(GiftPair('JohLen, 'GeoHar))), 'JohLen -> Player("John Lennon", Vector(GiftPair('PauMcc, 'RinSta))), 'GeoHar -> Player("George Harrison", Vector(GiftPair('RinSta, 'PauMcc))), 'PauMcc -> Player("Paul McCartney", Vector(GiftPair('GeoHar, 'JohLen))))
+  val ss = "The Beatles,2014\nRinSta,Ringo Starr,JohLen,GeoHar\nJohLen,John Lennon,PauMcc,RinSta\nGeoHar,George Harrison,RinSta,PauMcc\nPauMcc,Paul McCartney,GeoHar,JohLen"
+  //  val rl = List(List("The Beatles", "2014"), List("RinSta", "Ringo Starr", "JohLen", "GeoHar"), List("JohLen", "John Lennon", "PauMcc", "RinSta"), List("GeoHar", "George Harrison", "RinSta", "PauMcc"), List("PauMcc", "Paul McCartney", "GeoHar", "JohLen"))
+  //  val badRL = List(List(""))
+  //  val plist = List(List("RinSta", "Ringo Starr", "JohLen", "GeoHar"), List("JohLen", "John Lennon", "PauMcc", "RinSta"), List("GeoHar", "George Harrison", "RinSta", "PauMcc"), List("PauMcc", "Paul McCartney", "GeoHar", "JohLen"))
+  //  val pmap: redpoint.PlayersMap = Map('RinSta -> Player("Ringo Starr", Vector(GiftPair('JohLen, 'GeoHar))), 'JohLen -> Player("John Lennon", Vector(GiftPair('PauMcc, 'RinSta))), 'GeoHar -> Player("George Harrison", Vector(GiftPair('RinSta, 'PauMcc))), 'PauMcc -> Player("Paul McCartney", Vector(GiftPair('GeoHar, 'JohLen))))
 
-  "A RosterList" should "create itself properly" in {
-    assert(RosterUtility.makeRosterList(bs) == rl)
+  "A Roster" should "have a name" in {
+    assert(getRosterName(ss) == "The Beatles")
   }
 
-  "A RosterInfo" should "create itself properly" in {
-    assert(RosterUtility.makeRosterInfo(rl) == List("The Beatles", "2014"))
-    assert(RosterUtility.makeRosterInfo(badRL) == List("Is", "Empty"))
+  it should "have a year" in {
+    assert(getRosterYear(ss) == 2014)
   }
-
-  "A PlayersList" should "create itself properly" in {
-    assert(RosterUtility.makePlayersList(rl) == plist)
-    assert(RosterUtility.makePlayersList(badRL) == List(List("Is"), List("Empty")))
-  }
-
-  "A PlayersMap" should "create itself properly" in {
-    assert(RosterUtility.makePlayersMap(plist) == pmap)
-  }
-
-  "getPlayerInRoster" should "return Paul McCartney" in {
-    assert(RosterUtility.getPlayerInRoster('PauMcc)(pmap) == Player("Paul McCartney", Vector(GiftPair('GeoHar, 'JohLen))))
-  }
-
-  "A giftPair" should "update correctly" in {
-    assert(RosterUtility.setGiftPairInRoster('RinSta)(0)(GiftPair('RinSta, 'RinSta))(pmap) ==
-      Map('RinSta -> Player("Ringo Starr", Vector(GiftPair('RinSta, 'RinSta))),
-        'JohLen -> Player("John Lennon", Vector(GiftPair('PauMcc, 'RinSta))),
-        'GeoHar -> Player("George Harrison", Vector(GiftPair('RinSta, 'PauMcc))),
-        'PauMcc -> Player("Paul McCartney", Vector(GiftPair('GeoHar, 'JohLen)))))
-  }
+  //
+  //  "A RosterInfo" should "create itself properly" in {
+  //    assert(Roster.makeRosterInfo(rl) == List("The Beatles", "2014"))
+  //    assert(Roster.makeRosterInfo(badRL) == List("Is", "Empty"))
+  //  }
+  //
+  //  "A PlayersList" should "create itself properly" in {
+  //    assert(Roster.makePlayersList(rl) == plist)
+  //    assert(Roster.makePlayersList(badRL) == List(List("Is"), List("Empty")))
+  //  }
+  //
+  //  "A PlayersMap" should "create itself properly" in {
+  //    assert(Roster.makePlayersMap(plist) == pmap)
+  //  }
+  //
+  //  "getPlayerInRoster" should "return Paul McCartney" in {
+  //    assert(Roster.getPlayerInRoster('PauMcc)(pmap) == Player("Paul McCartney", Vector(GiftPair('GeoHar, 'JohLen))))
+  //  }
+  //
+  //  "A giftPair" should "update correctly" in {
+  //    assert(Roster.setGiftPairInRoster('RinSta)(0)(GiftPair('RinSta, 'RinSta))(pmap) ==
+  //      Map('RinSta -> Player("Ringo Starr", Vector(GiftPair('RinSta, 'RinSta))),
+  //        'JohLen -> Player("John Lennon", Vector(GiftPair('PauMcc, 'RinSta))),
+  //        'GeoHar -> Player("George Harrison", Vector(GiftPair('RinSta, 'PauMcc))),
+  //        'PauMcc -> Player("Paul McCartney", Vector(GiftPair('GeoHar, 'JohLen)))))
+  //  }
 }
