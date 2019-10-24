f525d3a72c293a9b61bf0b5d0a72670bdf945f55
Eric Tobin <eatobin@gmail.com>
2019-10-24 13:33:07 -0500

    fresh start 2.12.10

--- src/main/scala/redpoint/package.scala
+++ src/main/scala/redpoint/package.scala
@@ -1,24 +1,3 @@
 package object redpoint {
-  type RawString = String
-  type Scrubbed = String
-  type ErrorString = String
-  type ErrorOrScrubbed = Either[ErrorString, Scrubbed]
-  type EntityList = List[List[String]]
-  type PlayersList = List[List[String]]
-  type PlayerAsList = List[String]
-  type PlayersListSymbols = List[List[String]]
-  type PlayerAsListOfSymbols = List[String]
-
-  type RName = String
-  type RYear = Int
-  type PlrSym = Symbol
-  type Givee = PlrSym
-  type Giver = PlrSym
-  type PName = String
-  type GiftHist = Vector[GiftPair]
-  type PlayerLine = List[String]
-  type PlayerKV = (PlrSym, Player)
-  type PlayersKVList = List[(PlrSym, Player)]
-  type PlayersMap = Map[PlrSym, Player]
-  type GYear = Int
+  //  type RawString = String
 }
