bcbbb052c9a8a2e59b9d7c9a2541611328b0e8f7
Eric Tobin <eatobin@gmail.com>
2019-11-04 15:02:49 -0600

    format

--- src/main/scala/redpoint/Players.scala
+++ src/main/scala/redpoint/Players.scala
@@ -4,14 +4,10 @@ object Players {
   def getPlayer(players: Players, playerKey: PlayerKey): Player =
     players(playerKey)
 
-  def setPlayer(players: Players, playerKey: PlayerKey, player: Player): Players = {
+  def setPlayer(players: Players, playerKey: PlayerKey, player: Player): Players =
     players.updated(playerKey, player)
-  }
 
   def addYearPlayers(players: Players): Players =
-    for ((playerKey, player) <- players) yield {
-      {
-        playerKey -> Player.addYearPlayer(player, playerKey)
-      }
-    }
+    for ((playerKey, player) <- players) yield
+      playerKey -> Player.addYearPlayer(player, playerKey)
 }
