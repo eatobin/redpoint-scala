affdf11c6d23ef2372245b079140c2c1abecfb46
Eric Tobin <eatobin@gmail.com>
2019-11-03 20:48:17 -0600

    addYearPlayer

--- src/main/scala/redpoint/Player.scala
+++ src/main/scala/redpoint/Player.scala
@@ -5,4 +5,10 @@ case class Player(playerName: PlayerName, giftHistory: GiftHistory)
 object Player {
   def setGiftHistory(player: Player, giftHistory: GiftHistory): Player =
     player.copy(giftHistory = giftHistory)
+
+  def addYearPlayer(player: Player, playerKey: PlayerKey): Player = {
+    val gh = player.giftHistory
+    val ngh = GiftHistory.addYear(gh, playerKey)
+    setGiftHistory(player, ngh)
+  }
 }
