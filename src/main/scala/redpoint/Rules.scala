571f811f108af80846847e9523da3ac605717b7f
Eric Tobin <eatobin@gmail.com>
2019-11-19 15:58:06 -0600

    rules progress

--- src/main/scala/redpoint/Rules.scala
+++ src/main/scala/redpoint/Rules.scala
@@ -1,6 +1,13 @@
 package redpoint
 
+import redpoint.Players.getGivEeErPlayers
+
 object Rules {
-  def giveeNotSelf(playerKey: PlayerKey, givee: Givee): Boolean =
-    playerKey != givee
+  def giveeNotSelf(selfKey: PlayerKey, givee: Givee): Boolean =
+    selfKey != givee
+
+  def giveeNotRecip(selfKey: PlayerKey, givee: Givee, giftYear: GiftYear, players: Players): Boolean = {
+    val recip = getGivEeErPlayers(players, givee, giftYear, 'ee)
+    selfKey != recip
+  }
 }
--- src/test/scala/redpoint/RulesSpec.scala
+++ src/test/scala/redpoint/RulesSpec.scala
@@ -5,23 +5,25 @@ import redpoint.Rules._
 
 class RulesSpec extends FlatSpec {
 
-  private val rinSta: Player = Player("Ringo Starr", Vector(GiftPair('KarLav, 'JohLen)))
-  private val johLen: Player = Player("John Lennon", Vector(GiftPair('RinSta, 'GeoHar)))
-  private val geoHar: Player = Player("George Harrison", Vector(GiftPair('JohLen, 'PauMcc)))
-  private val pauMcc: Player = Player("Paul McCartney", Vector(GiftPair('GeoHar, 'EriTob)))
-  private val eriTob: Player = Player("Eric Tobin", Vector(GiftPair('PaulMcc, 'KarLav)))
-  private val karLav: Player = Player("Karen Lavengood", Vector(GiftPair('EriTob, 'RinSta)))
+  private val rinSta: Player = Player("Ringo Starr", Vector(GiftPair(giver = 'KarLav, givee = 'JohLen)))
+  private val johLen: Player = Player("John Lennon", Vector(GiftPair(giver = 'RinSta, givee = 'GeoHar)))
+  private val geoHar: Player = Player("George Harrison", Vector(GiftPair(giver = 'JohLen, givee = 'PauMcc)))
+  private val pauMcc: Player = Player("Paul McCartney", Vector(GiftPair(giver = 'GeoHar, givee = 'EriTob)))
+  private val eriTob: Player = Player("Eric Tobin", Vector(GiftPair(giver = 'PaulMcc, givee = 'KarLav)))
+  private val karLav: Player = Player("Karen Lavengood", Vector(GiftPair(giver = 'EriTob, givee = 'RinSta)))
   private val beatlesPlusPM: Players =
     Map('RinSta -> rinSta, 'JohLen -> johLen, 'GeoHar -> geoHar, 'PauMcc -> pauMcc, 'EriTob -> eriTob, 'KarLav -> karLav)
 
   "A Player" should "not give to itself" in {
-    assert(giveeNotSelf('RinSta, 'GeoHar) == true)
+    assert(giveeNotSelf('RinSta, 'GeoHar))
+    assert(!giveeNotSelf('RinSta, 'RinSta))
   }
 
-  //  it should "return its giftHistory" in {
-  //    assert(player.giftHistory == Vector(GiftPair('JohLen, 'GeoHar)))
-  //  }
-  //
+  it should "not give to it's recip" in {
+    assert(giveeNotRecip('RinSta, 'JohLen, 0, beatlesPlusPM))
+    assert(!giveeNotRecip('RinSta, 'KarLav, 0, beatlesPlusPM))
+  }
+
   //  it should "return an updated giftHistory" in {
   //    assert(setGiftHistory(player, Vector(GiftPair('nope, 'yup))) ==
   //      Player("Ringo Starr", Vector(GiftPair('nope, 'yup))))
