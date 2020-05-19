f8d9ee464b35b68b7ed89fc9434b4ff74d59d6b2
Eric Tobin <eatobin@gmail.com>
2020-05-18 20:43:49 -0500

    progress

--- src/main/scala/redpoint/GiftHistory.scala
+++ src/main/scala/redpoint/GiftHistory.scala
@@ -4,9 +4,6 @@ object GiftHistory {
   def giftHistoryAddYear(giftHistory: GiftHistory, playerKey: PlayerKey): GiftHistory =
     giftHistory :+ GiftPair(playerKey, playerKey)
 
-//  def getGiftPair(giftHistory: GiftHistory, giftYear: GiftYear): GiftPair =
-//    giftHistory(giftYear)
-
   def giftHistoryUpdateGiftPair(giftHistory: GiftHistory, giftYear: GiftYear, giftPair: GiftPair): GiftHistory = {
     giftHistory.updated(giftYear, giftPair)
   }
--- src/main/scala/redpoint/GiftPair.scala
+++ src/main/scala/redpoint/GiftPair.scala
@@ -3,10 +3,6 @@ package redpoint
 case class GiftPair(givee: Givee, giver: Giver)
 
 object GiftPair {
-//  def getGivee(giftPair: GiftPair): Givee = giftPair.givee
-//
-//  def getGiver(giftPair: GiftPair): Giver = giftPair.giver
-
   def giftPairSetGivee(giftPair: GiftPair, newGivee: Givee): GiftPair = giftPair.copy(givee = newGivee)
 
   def giftPairSetGiver(giftPair: GiftPair, newGiver: Giver): GiftPair = giftPair.copy(giver = newGiver)
