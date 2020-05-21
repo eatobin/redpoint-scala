b2f4d337b07cbc34d636f13f3bf2a2569e2e3d52
Eric Tobin <eatobin@gmail.com>
2020-05-20 20:44:39 -0500

    Roster.scala progress

--- src/main/scala/redpoint/Roster.scala
+++ src/main/scala/redpoint/Roster.scala
@@ -4,3 +4,9 @@
 package redpoint
 
 case class Roster(rosterName: RosterName, rosterYear: RosterYear, players: Players)
+
+object Roster {
+  def getPlayerName(roster: Roster, playerKey: PlayerKey): PlayerName = {
+    roster.players(playerKey).playerName
+  }
+}
