2dc3ee6025903fe7e8dea4fb09d86f7ef2c4010f
Eric Tobin <eatobin@gmail.com>
2020-05-23 09:02:58 -0500

    TODO

--- src/main/scala/redpoint/GiftHistory.scala
+++ src/main/scala/redpoint/GiftHistory.scala
@@ -3,7 +3,7 @@ package redpoint
 object GiftHistory {
   def giftHistoryAddYear(giftHistory: GiftHistory, playerKey: PlayerKey): GiftHistory =
     giftHistory :+ GiftPair(playerKey, playerKey)
-
+// TODO: gift-history-update-gift-history
   def giftHistoryUpdateGiftPair(giftHistory: GiftHistory, giftYear: GiftYear, giftPair: GiftPair): GiftHistory = {
     giftHistory.updated(giftYear, giftPair)
   }
