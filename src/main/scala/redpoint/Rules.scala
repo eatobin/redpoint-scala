5d49159d5dd294ed6a896b4522ebe718cda15ffd
Eric Tobin <eatobin@gmail.com>
2020-07-09 21:09:50 -0500

    rules progress

--- src/main/scala/redpoint/Rules.scala
+++ src/main/scala/redpoint/Rules.scala
@@ -1,20 +1,18 @@
-//package redpoint
-//
-//import redpoint.Players.getGivEeErPlayers
-//
-//object Rules {
-//  def giveeNotSelf(selfKey: PlayerKey, givee: Givee): Boolean =
-//    selfKey != givee
-//
-//  def giveeNotRecip(selfKey: PlayerKey, givee: Givee, giftYear: GiftYear, players: Players): Boolean = {
-//    val recip = getGivEeErPlayers(players, givee, 'ee, giftYear)
-//    selfKey != recip
-//  }
-//
-//  def giveeNotRepeat(selfKey: PlayerKey, givee: Givee, giftYear: GiftYear, players: Players): Boolean = {
-//    val past = (giftYear - 1).to(giftYear - 3).by(-1).toVector.filterNot(y => y < 0)
-//    val geY = getGivEeErPlayers(players, selfKey, 'ee, _: GiftYear)
-//    val geInYrs = past.map(gy => geY(gy))
-//    !geInYrs.contains(givee)
-//  }
-//}
+package redpoint
+
+object Rules {
+  def giveeNotSelf(selfKey: PlayerKey, givee: Givee): Boolean =
+    selfKey != givee
+
+  def giveeNotRecip(selfKey: PlayerKey, givee: Givee, giftYear: GiftYear, players: Players): Boolean = {
+    val recip = Roster.getGivee(players, givee, giftYear)
+    selfKey != recip
+  }
+
+  def giveeNotRepeat(selfKey: PlayerKey, givee: Givee, giftYear: GiftYear, players: Players): Boolean = {
+    val past = (giftYear - 1).to(giftYear - 3).by(-1).toVector.filterNot(y => y < 0)
+    val geY = Roster.getGivee(players, selfKey, _: GiftYear)
+    val geInYrs = past.map(gy => geY(gy))
+    !geInYrs.contains(givee)
+  }
+}
