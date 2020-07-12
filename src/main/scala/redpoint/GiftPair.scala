e938441c65250db1d67f274645b7f973cd7a38d5
Eric Tobin <eatobin@gmail.com>
2020-07-12 16:32:56 -0500

    json progress

--- src/main/scala/redpoint/GiftPair.scala
+++ src/main/scala/redpoint/GiftPair.scala
@@ -1,9 +1,14 @@
 package redpoint
 
+import spray.json._
+
 case class GiftPair(givee: Givee, giver: Giver)
 
-object GiftPair {
+object GiftPair extends DefaultJsonProtocol {
+
   def giftPairUpdateGivee(giftPair: GiftPair, newGivee: Givee): GiftPair = giftPair.copy(givee = newGivee)
 
   def giftPairUpdateGiver(giftPair: GiftPair, newGiver: Giver): GiftPair = giftPair.copy(giver = newGiver)
+
+  implicit val giftPairFormat: RootJsonFormat[GiftPair] = jsonFormat2(GiftPair.apply)
 }
