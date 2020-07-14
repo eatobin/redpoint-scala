96d47c3c4e0e6343db0ab325ff1bc1c8bb434276
Eric Tobin <eatobin@gmail.com>
2020-07-14 18:05:06 -0500

    JsRoster progress

--- src/test/scala/redpoint/RosterSpec.scala
+++ src/test/scala/redpoint/RosterSpec.scala
@@ -14,6 +14,8 @@ class RosterSpec extends AnyFlatSpec {
     Map(Symbol("RinSta") -> rinSta, Symbol("JohLen") -> johLen, Symbol("GeoHar") -> geoHar, Symbol("PauMcc") -> pauMcc)
   private val roster: Roster = Roster("The Beatles", 2014, players)
 
+  private val jsBeatlesBad: JsRoster = """{  "roster-name": "The Beatles",  "roster-year": 2014,  "players": {    "RinSta": {      "player-name": "Ringo Starr",      "gift-history": [        {          "givee": "JohLen",          "giver": "GeoHar"        }      ]    }  },  "JohLen": {    "player-name": "John Lennon",    "gift-history": [      {        "givee": "PauMcc",        "giver": "RinSta"      }    ]  },  "GeoHar": {    "player-name": "George Harrison",    "gift-history": [      {        "givee": "RinSta",        "giver": "PauMcc"      }    ]  },  "PauMcc": {    "player-name": "Paul McCartney",    "gift-history": [      {        "givee": "GeoHar",        "giver": "JohLen"      }    ]  }}""".parseJson
+
   "A Roster" should "return \"The Beatles\" rosterName" in {
     assert(roster.rosterName == "The Beatles")
   }
@@ -22,18 +24,10 @@ class RosterSpec extends AnyFlatSpec {
     assert(roster.rosterYear == 2014)
   }
 
-  it should "convert to JSON" in {
-    val rosterJson: JsRoster = roster.toJson
-    assert(rosterJson == """{"players":{"RinSta":{"giftHistory":[{"givee":"JohLen","giver":"GeoHar"}],"playerName":"Ringo Starr"},"JohLen":{"giftHistory":[{"givee":"PauMcc","giver":"RinSta"}],"playerName":"John Lennon"},"GeoHar":{"giftHistory":[{"givee":"RinSta","giver":"PauMcc"}],"playerName":"George Harrison"},"PauMcc":{"giftHistory":[{"givee":"GeoHar","giver":"JohLen"}],"playerName":"Paul McCartney"}},"rosterName":"The Beatles","rosterYear":2014}""".parseJson)
-  }
-
-  it should "convert from JSON" in {
-    val rosterJson: JsRoster = roster.toJson
-    assert(rosterJson.convertTo[Roster] == roster)
-  }
-
-  it should "convert a JSON string to a Roster" in {
+  it should "convert a JSON string to a Roster - or not" in {
     val rosterJson: JsRoster = roster.toJson
     assert(jsonStringToRoster(Right(rosterJson)) == Right(roster))
+    assert(jsonStringToRoster(Right(jsBeatlesBad)) == Left("JSON parse error."))
+    assert(jsonStringToRoster(Left("nope")) == Left("nope"))
   }
 }
