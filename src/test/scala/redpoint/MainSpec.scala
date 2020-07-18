e6a033e4735ade57c8e559cd5d3f9398f45bbcc8
Eric Tobin <eatobin@gmail.com>
2020-07-18 18:25:58 -0500

    rename

--- src/test/scala/redpoint/MainSpec.scala
+++ src/test/scala/redpoint/MainSpec.scala
@@ -10,7 +10,7 @@ class MainSpec extends AnyFlatSpec {
   private val jsBeatles: JsRoster = """{  "roster-name": "The Beatles",  "roster-year": 2014,  "players": {    "RinSta": {      "player-name": "Ringo Starr",      "gift-history": [        {          "givee": "JohLen",          "giver": "GeoHar"        }      ]    }  },  "JohLen": {    "player-name": "John Lennon",    "gift-history": [      {        "givee": "PauMcc",        "giver": "RinSta"      }    ]  },  "GeoHar": {    "player-name": "George Harrison",    "gift-history": [      {        "givee": "RinSta",        "giver": "PauMcc"      }    ]  },  "PauMcc": {    "player-name": "Paul McCartney",    "gift-history": [      {        "givee": "GeoHar",        "giver": "JohLen"      }    ]  }}""".parseJson
 
   "Main" should "return a Roster as a String" in {
-    assert(readFileIntoJsonString(fp) == Right(jsBeatles))
-    assert(readFileIntoJsonString("src/test/resources/none.json") == Left("File read error. File: " ++ "src/test/resources/none.json" ++ " does not exist."))
+    assert(readFileIntoJsRoster(fp) == Right(jsBeatles))
+    assert(readFileIntoJsRoster("src/test/resources/none.json") == Left("File read error. File: " ++ "src/test/resources/none.json" ++ " does not exist."))
   }
 }
