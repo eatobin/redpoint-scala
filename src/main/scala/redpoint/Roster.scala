17f0995263f63bd442a51ab2b0ccc59872baa591
Eric Tobin <eatobin@gmail.com>
2020-09-25 21:44:39 -0500

    all pass

--- src/main/scala/redpoint/Roster.scala
+++ src/main/scala/redpoint/Roster.scala
@@ -5,11 +5,11 @@ import spray.json._
 case class Roster(rosterName: RosterName, rosterYear: RosterYear, players: Players)
 
 object Roster extends DefaultJsonProtocol {
-  def jsValueToRoster(s: Either[ErrorString, JsValue]): Either[ErrorString, Roster] = {
+  def rosterJsonStringToRoster(s: Either[ErrorString, String]): Either[ErrorString, Roster] = {
     s match {
       case Right(r) =>
         try {
-          Right(r.convertTo[Roster])
+          Right(r.parseJson.convertTo[Roster])
         } catch {
           case _: Exception =>
             Left("JSON parse error.")
--- src/test/scala/redpoint/RosterSpec.scala
+++ src/test/scala/redpoint/RosterSpec.scala
@@ -6,6 +6,8 @@ import spray.json._
 
 class RosterSpec extends AnyFlatSpec {
 
+  private val jsonStringRos: String = """{"players":{"RinSta":{"giftHistory":[{"givee":"JohLen","giver":"GeoHar"}],"playerName":"Ringo Starr"},"JohLen":{"giftHistory":[{"givee":"PauMcc","giver":"RinSta"}],"playerName":"John Lennon"},"GeoHar":{"giftHistory":[{"givee":"RinSta","giver":"PauMcc"}],"playerName":"George Harrison"},"PauMcc":{"giftHistory":[{"givee":"GeoHar","giver":"JohLen"}],"playerName":"Paul McCartney"}},"rosterName":"The Beatles","rosterYear":2014}"""
+
   private val rinSta: Player = Player("Ringo Starr", Vector(GiftPair(Symbol("JohLen"), Symbol("GeoHar"))))
   private val johLen: Player = Player("John Lennon", Vector(GiftPair(Symbol("PauMcc"), Symbol("RinSta"))))
   private val geoHar: Player = Player("George Harrison", Vector(GiftPair(Symbol("RinSta"), Symbol("PauMcc"))))
@@ -14,7 +16,7 @@ class RosterSpec extends AnyFlatSpec {
     Map(Symbol("RinSta") -> rinSta, Symbol("JohLen") -> johLen, Symbol("GeoHar") -> geoHar, Symbol("PauMcc") -> pauMcc)
   private val roster: Roster = Roster("The Beatles", 2014, players)
 
-  private val jsBeatlesBad: JsValue = """{  "roster-name": "The Beatles",  "roster-year": 2014,  "players": {    "RinSta": {      "player-name": "Ringo Starr",      "gift-history": [        {          "givee": "JohLen",          "giver": "GeoHar"        }      ]    }  },  "JohLen": {    "player-name": "John Lennon",    "gift-history": [      {        "givee": "PauMcc",        "giver": "RinSta"      }    ]  },  "GeoHar": {    "player-name": "George Harrison",    "gift-history": [      {        "givee": "RinSta",        "giver": "PauMcc"      }    ]  },  "PauMcc": {    "player-name": "Paul McCartney",    "gift-history": [      {        "givee": "GeoHar",        "giver": "JohLen"      }    ]  }}""".parseJson
+  private val jsBeatlesBad: String = """{  "roster-name": "The Beatles",  "roster-year": 2014,  "players": {    "RinSta": {      "player-name": "Ringo Starr",      "gift-history": [        {          "givee": "JohLen",          "giver": "GeoHar"        }      ]    }  },  "JohLen": {    "player-name": "John Lennon",    "gift-history": [      {        "givee": "PauMcc",        "giver": "RinSta"      }    ]  },  "GeoHar": {    "player-name": "George Harrison",    "gift-history": [      {        "givee": "RinSta",        "giver": "PauMcc"      }    ]  },  "PauMcc": {    "player-name": "Paul McCartney",    "gift-history": [      {        "givee": "GeoHar",        "giver": "JohLen"      }    ]  }}"""
 
   "A Roster" should "return \"The Beatles\" rosterName" in {
     assert(roster.rosterName == "The Beatles")
@@ -24,10 +26,12 @@ class RosterSpec extends AnyFlatSpec {
     assert(roster.rosterYear == 2014)
   }
 
-  it should "convert a JSON value to a Roster - or not" in {
-    val rosterJson: JsValue = roster.toJson
-    assert(jsValueToRoster(Right(rosterJson)) == Right(roster))
-    assert(jsValueToRoster(Right(jsBeatlesBad)) == Left("JSON parse error."))
-    assert(jsValueToRoster(Left("nope")) == Left("nope"))
+  it should "convert from JSON - or not" in {
+    val rosJson: Either[ErrorString, Roster] = rosterJsonStringToRoster(Right(jsonStringRos))
+    val rosJsonBad: Either[ErrorString, Roster] = rosterJsonStringToRoster(Right(jsBeatlesBad))
+    val rosJsonNoFile: Either[ErrorString, Roster] = rosterJsonStringToRoster(Left("nope"))
+    assert(rosJson == Right(roster))
+    assert(rosJsonBad == Left("JSON parse error."))
+    assert(rosJsonNoFile == Left("nope"))
   }
 }
