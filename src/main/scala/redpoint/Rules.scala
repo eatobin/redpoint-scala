e61300626b952863850c2c94260727a280102da3
Eric Tobin <eatobin@gmail.com>
2021-02-18 11:19:08 -0600

    rules tests!

--- src/main/scala/redpoint/Rules.scala
+++ src/main/scala/redpoint/Rules.scala
@@ -1,20 +1,20 @@
 package redpoint
 
-//import redpoint.Players.playersGetGivee
-//
-//object Rules {
-//  def giveeNotSelf(selfKey: Symbol, givee: Symbol): Boolean =
-//    selfKey != givee
-//
-//  def giveeNotRecip(selfKey: Symbol, givee: Symbol, giftYear: Int, players: Map[Symbol, Player]): Boolean = {
-//    val recip = playersGetGivee(players, givee, giftYear)
-//    selfKey != recip
-//  }
-//
-//  def giveeNotRepeat(selfKey: Symbol, givee: Symbol, giftYear: Int, players: Map[Symbol, Player]): Boolean = {
-//    val past = (giftYear - 1).to(giftYear - 3).by(-1).toVector.filterNot(y => y < 0)
-//    val geY = playersGetGivee(players, selfKey, _: Int)
-//    val geInYrs = past.map(gy => geY(gy))
-//    !geInYrs.contains(givee)
-//  }
-//}
+import redpoint.Players.playersGetGivee
+
+object Rules {
+  def giveeNotSelf(selfKey: String, givee: String): Boolean =
+    selfKey != givee
+
+  def giveeNotRecip(selfKey: String, givee: String, giftYear: Int, players: Map[String, Player]): Boolean = {
+    val recip = playersGetGivee(players, givee, giftYear)
+    selfKey != recip
+  }
+
+  def giveeNotRepeat(selfKey: String, givee: String, giftYear: Int, players: Map[String, Player]): Boolean = {
+    val past = (giftYear - 1).to(giftYear - 3).by(-1).toVector.filterNot(y => y < 0)
+    val geY = playersGetGivee(players, selfKey, _: Int)
+    val geInYrs = past.map(gy => geY(gy))
+    !geInYrs.contains(givee)
+  }
+}
--- src/test/scala/redpoint/RulesSpec.scala
+++ src/test/scala/redpoint/RulesSpec.scala
@@ -1,46 +1,46 @@
 package redpoint
 
-//import org.scalatest.flatspec.AnyFlatSpec
-//import redpoint.Rules._
-//
-//class RulesSpec extends AnyFlatSpec {
-//
-//  private val rinSta: Player = Player("Ringo Starr", Vector(GiftPair(giver = Symbol("KarLav"), givee = Symbol("JohLen"))))
-//  private val johLen: Player = Player("John Lennon", Vector(GiftPair(giver = Symbol("RinSta"), givee = Symbol("GeoHar"))))
-//  private val geoHar: Player = Player("George Harrison", Vector(GiftPair(giver = Symbol("JohLen"), givee = Symbol("PauMcc"))))
-//  private val pauMcc: Player = Player("Paul McCartney", Vector(GiftPair(giver = Symbol("GeoHar"), givee = Symbol("EriTob"))))
-//  private val eriTob: Player = Player("Eric Tobin", Vector(GiftPair(giver = Symbol("PaulMcc"), givee = Symbol("KarLav"))))
-//  private val karLav: Player = Player("Karen Lavengood", Vector(GiftPair(giver = Symbol("EriTob"), givee = Symbol("RinSta"))))
-//  private val beatlesPlusPM: Map[Symbol, Player] =
-//    Map(Symbol("RinSta") -> rinSta, Symbol("JohLen") -> johLen, Symbol("GeoHar") -> geoHar, Symbol("PauMcc") -> pauMcc, Symbol("EriTob") -> eriTob, Symbol("KarLav") -> karLav)
-//
-//  private var beatlesPlus4 = Players.playersAddYear(beatlesPlusPM)
-//  beatlesPlus4 = Players.playersUpdateGivee(beatlesPlus4, Symbol("RinSta"), 1, Symbol("GeoHar"))
-//  beatlesPlus4 = Players.playersAddYear(beatlesPlus4)
-//  beatlesPlus4 = Players.playersUpdateGivee(beatlesPlus4, Symbol("RinSta"), 2, Symbol("PauMcc"))
-//  beatlesPlus4 = Players.playersAddYear(beatlesPlus4)
-//  beatlesPlus4 = Players.playersUpdateGivee(beatlesPlus4, Symbol("RinSta"), 3, Symbol("EriTob"))
-//  beatlesPlus4 = Players.playersAddYear(beatlesPlus4)
-//  beatlesPlus4 = Players.playersUpdateGivee(beatlesPlus4, Symbol("RinSta"), 4, Symbol("KarLav"))
-//
-//  "A Player" should "not give to itself" in {
-//    assert(giveeNotSelf(Symbol("RinSta"), Symbol("GeoHar")))
-//    assert(!giveeNotSelf(Symbol("RinSta"), Symbol("RinSta")))
-//  }
-//
-//  it should "not give to it's recip" in {
-//    assert(giveeNotRecip(Symbol("RinSta"), Symbol("JohLen"), 0, beatlesPlusPM))
-//    assert(!giveeNotRecip(Symbol("RinSta"), Symbol("KarLav"), 0, beatlesPlusPM))
-//  }
-//
-//  it should "not repeat for three years" in {
-//    assert(!giveeNotRepeat(Symbol("RinSta"), Symbol("JohLen"), 2, beatlesPlus4))
-//    assert(!giveeNotRepeat(Symbol("RinSta"), Symbol("GeoHar"), 2, beatlesPlus4))
-//    assert(giveeNotRepeat(Symbol("RinSta"), Symbol("KarLav"), 2, beatlesPlus4))
-//    assert(giveeNotRepeat(Symbol("RinSta"), Symbol("JohLen"), 5, beatlesPlus4))
-//    assert(giveeNotRepeat(Symbol("RinSta"), Symbol("GeoHar"), 5, beatlesPlus4))
-//    assert(!giveeNotRepeat(Symbol("RinSta"), Symbol("PauMcc"), 5, beatlesPlus4))
-//    assert(!giveeNotRepeat(Symbol("RinSta"), Symbol("EriTob"), 5, beatlesPlus4))
-//    assert(!giveeNotRepeat(Symbol("RinSta"), Symbol("KarLav"), 5, beatlesPlus4))
-//  }
-//}
+import org.scalatest.flatspec.AnyFlatSpec
+import redpoint.Rules._
+
+class RulesSpec extends AnyFlatSpec {
+
+  private val rinSta: Player = Player("Ringo Starr", Vector(GiftPair(giver = "KarLav", givee = "JohLen")))
+  private val johLen: Player = Player("John Lennon", Vector(GiftPair(giver = "RinSta", givee = "GeoHar")))
+  private val geoHar: Player = Player("George Harrison", Vector(GiftPair(giver = "JohLen", givee = "PauMcc")))
+  private val pauMcc: Player = Player("Paul McCartney", Vector(GiftPair(giver = "GeoHar", givee = "EriTob")))
+  private val eriTob: Player = Player("Eric Tobin", Vector(GiftPair(giver = "PaulMcc", givee = "KarLav")))
+  private val karLav: Player = Player("Karen Lavengood", Vector(GiftPair(giver = "EriTob", givee = "RinSta")))
+  private val beatlesPlusPM: Map[String, Player] =
+    Map("RinSta" -> rinSta, "JohLen" -> johLen, "GeoHar" -> geoHar, "PauMcc" -> pauMcc, "EriTob" -> eriTob, "KarLav" -> karLav)
+
+  private var beatlesPlus4 = Players.playersAddYear(beatlesPlusPM)
+  beatlesPlus4 = Players.playersUpdateGivee(beatlesPlus4, "RinSta", 1, "GeoHar")
+  beatlesPlus4 = Players.playersAddYear(beatlesPlus4)
+  beatlesPlus4 = Players.playersUpdateGivee(beatlesPlus4, "RinSta", 2, "PauMcc")
+  beatlesPlus4 = Players.playersAddYear(beatlesPlus4)
+  beatlesPlus4 = Players.playersUpdateGivee(beatlesPlus4, "RinSta", 3, "EriTob")
+  beatlesPlus4 = Players.playersAddYear(beatlesPlus4)
+  beatlesPlus4 = Players.playersUpdateGivee(beatlesPlus4, "RinSta", 4, "KarLav")
+
+  "A Player" should "not give to itself" in {
+    assert(giveeNotSelf("RinSta", "GeoHar"))
+    assert(!giveeNotSelf("RinSta", "RinSta"))
+  }
+
+  it should "not give to it's recip" in {
+    assert(giveeNotRecip("RinSta", "JohLen", 0, beatlesPlusPM))
+    assert(!giveeNotRecip("RinSta", "KarLav", 0, beatlesPlusPM))
+  }
+
+  it should "not repeat for three years" in {
+    assert(!giveeNotRepeat("RinSta", "JohLen", 2, beatlesPlus4))
+    assert(!giveeNotRepeat("RinSta", "GeoHar", 2, beatlesPlus4))
+    assert(giveeNotRepeat("RinSta", "KarLav", 2, beatlesPlus4))
+    assert(giveeNotRepeat("RinSta", "JohLen", 5, beatlesPlus4))
+    assert(giveeNotRepeat("RinSta", "GeoHar", 5, beatlesPlus4))
+    assert(!giveeNotRepeat("RinSta", "PauMcc", 5, beatlesPlus4))
+    assert(!giveeNotRepeat("RinSta", "EriTob", 5, beatlesPlus4))
+    assert(!giveeNotRepeat("RinSta", "KarLav", 5, beatlesPlus4))
+  }
+}
