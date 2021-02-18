92a5890627b2696f6c6d6a163daa14c1234a87c9
Eric Tobin <eatobin@gmail.com>
2021-02-18 10:27:16 -0600

    roster

--- src/main/scala/redpoint/Roster.scala
+++ src/main/scala/redpoint/Roster.scala
@@ -1,23 +1,26 @@
 package redpoint
 
-//import spray.json._
-//
-//case class Roster(rosterName: String, rosterYear: Int, players: Map[Symbol, Player])
-//
-//object Roster extends DefaultJsonProtocol {
-//  def rosterJsonStringToRoster(s: Either[ErrorString, JsonString]): Either[ErrorString, Roster] = {
-//    s match {
-//      case Right(r) =>
-//        try {
-//          Right(r.parseJson.convertTo[Roster])
-//        } catch {
-//          case e: Exception =>
-//            Left(e.getMessage)
-//        }
-//      case Left(l) =>
-//        Left(l)
-//    }
-//  }
-//
-//  implicit val rosterFormat: RootJsonFormat[Roster] = jsonFormat3(Roster.apply)
-//}
+import io.circe.generic.auto._
+import io.circe.parser._
+
+case class Roster(rosterName: String, rosterYear: Int, players: Map[Symbol, Player])
+
+object Roster {
+  def rosterJsonStringToRoster(s: Either[ErrorString, JsonString]): Either[ErrorString, Roster] = {
+    s match {
+      case Right(rFile) =>
+        try {
+          val res = decode[Roster](rFile)
+          res match {
+            case Right(rDecode) => Right(rDecode)
+            case Left(lDecode) => Left(lDecode.toString)
+          }
+        } catch {
+          case eFile: Exception =>
+            Left(eFile.getMessage)
+        }
+      case Left(l) =>
+        Left(l)
+    }
+  }
+}
