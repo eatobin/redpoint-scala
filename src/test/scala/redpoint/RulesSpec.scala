f1e954272b6b84fac2e75c0161c2e5553eb07b16
Eric Tobin <eatobin@gmail.com>
2021-02-18 11:32:50 -0600

    main tests!

--- src/test/scala/redpoint/RulesSpec.scala
+++ src/test/scala/redpoint/RulesSpec.scala
@@ -9,7 +9,7 @@ class RulesSpec extends AnyFlatSpec {
   private val johLen: Player = Player("John Lennon", Vector(GiftPair(giver = "RinSta", givee = "GeoHar")))
   private val geoHar: Player = Player("George Harrison", Vector(GiftPair(giver = "JohLen", givee = "PauMcc")))
   private val pauMcc: Player = Player("Paul McCartney", Vector(GiftPair(giver = "GeoHar", givee = "EriTob")))
-  private val eriTob: Player = Player("Eric Tobin", Vector(GiftPair(giver = "PaulMcc", givee = "KarLav")))
+  private val eriTob: Player = Player("Eric Tobin", Vector(GiftPair(giver = "PauMcc", givee = "KarLav")))
   private val karLav: Player = Player("Karen Lavengood", Vector(GiftPair(giver = "EriTob", givee = "RinSta")))
   private val beatlesPlusPM: Map[String, Player] =
     Map("RinSta" -> rinSta, "JohLen" -> johLen, "GeoHar" -> geoHar, "PauMcc" -> pauMcc, "EriTob" -> eriTob, "KarLav" -> karLav)
