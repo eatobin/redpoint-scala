e1039f8c2c4854a9c3499daa459dc8db415d1382
Eric Tobin <eatobin@gmail.com>
2021-02-17 20:36:38 -0600

    on to circe + bye Symbols!

--- src/main/scala/redpoint/Player.scala
+++ src/main/scala/redpoint/Player.scala
@@ -1,14 +1,15 @@
 package redpoint
 
-//import spray.json._
-//
-//case class Player(playerName: String, giftHistory: Vector[GiftPair])
-//
-//object Player extends DefaultJsonProtocol {
-//  def playerUpdateGiftHistory(player: Player, giftHistory: Vector[GiftPair]): Player =
-//    player.copy(giftHistory = giftHistory)
-//
-//  def playerJsonStringToPlayer(plrString: String): Player = plrString.parseJson.convertTo[Player]
-//
-//  implicit val playerFormat: RootJsonFormat[Player] = jsonFormat2(Player.apply)
-//}
+import io.circe.Error
+import io.circe.generic.auto._
+import io.circe.parser._
+
+case class Player(playerName: String, giftHistory: Vector[GiftPair])
+
+object Player {
+  def playerUpdateGiftHistory(player: Player, giftHistory: Vector[GiftPair]): Player =
+    player.copy(giftHistory = giftHistory)
+
+  def playerJsonStringToPlayer(plrString: String): Either[Error, Player] =
+    decode[Player](plrString)
+}
