1f18eae54b84537ca87c05ec0277e80f0d7c673a
Eric Tobin <eatobin@gmail.com>
2021-03-14 20:39:01 -0500

    player tests

--- src/main/scala/redpoint/Player.scala
+++ src/main/scala/redpoint/Player.scala
@@ -1,15 +1,19 @@
-//package redpoint
-//
-//import io.circe.Error
-//import io.circe.generic.auto._
-//import io.circe.parser._
-//
-//case class Player(playerName: String, giftHistory: Vector[GiftPair])
-//
-//object Player {
-//  def playerUpdateGiftHistory(player: Player, giftHistory: Vector[GiftPair]): Player =
-//    player.copy(giftHistory = giftHistory)
-//
-//  def playerJsonStringToPlayer(plrString: String): Either[Error, Player] =
-//    decode[Player](plrString)
-//}
+package redpoint
+
+import io.circe.Error
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax.EncoderOps
+
+case class Player(playerName: String, giftHistory: Vector[GiftPair])
+
+object Player {
+  def playerUpdateGiftHistory(giftHistory: Vector[GiftPair], player: Player): Player =
+    player.copy(giftHistory = giftHistory)
+
+  def playerJsonStringToPlayer(plrString: String): Either[Error, Player] =
+    decode[Player](plrString)
+
+  def playerToJsonString(player: Player): JsonString =
+    player.asJson.noSpaces
+}
