706bc4da8cb8973632e0caeb7c68e1463afd01ad
Eric Tobin <eatobin@gmail.com>
2021-03-14 20:26:49 -0500

    gh tests

--- src/main/scala/redpoint/GiftHistory.scala
+++ src/main/scala/redpoint/GiftHistory.scala
@@ -1,17 +1,20 @@
-//package redpoint
-//
-//import io.circe.Error
-//import io.circe.generic.auto._
-//import io.circe.parser._
-//import io.circe.syntax.EncoderOps
-//
-//object GiftHistory {
-//  def giftHistoryAddYear(giftHistory: Vector[GiftPair])(playerKey: String): Vector[GiftPair] =
-//    giftHistory :+ GiftPair(playerKey, playerKey)
-//
-//  def giftHistoryUpdateGiftHistory(giftHistory: Vector[GiftPair], giftYear: Int, giftPair: GiftPair): Vector[GiftPair] =
-//    giftHistory.updated(giftYear, giftPair)
-//
-//  def giftHistoryJsonStringToGiftHistory(ghString: String): Either[Error, Vector[GiftPair]] =
-//    decode[Vector[GiftPair]](ghString)
-//}
+package redpoint
+
+import io.circe.Error
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax.EncoderOps
+
+object GiftHistory {
+  def giftHistoryAddYear(playerKey: String)(giftHistory: Vector[GiftPair]): Vector[GiftPair] =
+    giftHistory :+ GiftPair(playerKey, playerKey)
+
+  def giftHistoryUpdateGiftHistory(giftYear: Int)(giftPair: GiftPair)(giftHistory: Vector[GiftPair]): Vector[GiftPair] =
+    giftHistory.updated(giftYear, giftPair)
+
+  def giftHistoryJsonStringToGiftHistory(ghString: String): Either[Error, Vector[GiftPair]] =
+    decode[Vector[GiftPair]](ghString)
+
+  def giftHistoryToJsonString(giftHistory: Vector[GiftPair]): JsonString =
+    giftHistory.asJson.noSpaces
+}
--- src/test/scala/redpoint/GiftHistorySpec.scala
+++ src/test/scala/redpoint/GiftHistorySpec.scala
@@ -1,24 +1,29 @@
-//package redpoint
-//
-//import io.circe.Error
-//import org.scalatest.flatspec.AnyFlatSpec
-//import redpoint.GiftHistory._
-//
-//class GiftHistorySpec extends AnyFlatSpec {
-//
-//  private val jsonStringGH: JsonString = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
-//  private val giftHistory: Vector[GiftPair] = Vector(GiftPair("GeoHar", "JohLen"))
-//
-//  "A GiftHistory" should "add a new year" in {
-//    assert(giftHistoryAddYear(giftHistory, "NewBee") == Vector(GiftPair("GeoHar", "JohLen"), GiftPair("NewBee", "NewBee")))
-//  }
-//
-//  it should "return an updated giftHistory" in {
-//    assert(giftHistoryUpdateGiftHistory(giftHistory, 0, GiftPair("me", "you")) == Vector(GiftPair("me", "you")))
-//  }
-//
-//  it should "convert from JSON" in {
-//    val ghJson: Either[Error, Vector[GiftPair]] = giftHistoryJsonStringToGiftHistory(jsonStringGH)
-//    assert(ghJson == Right(giftHistory))
-//  }
-//}
+package redpoint
+
+import io.circe.Error
+import org.scalatest.flatspec.AnyFlatSpec
+import redpoint.GiftHistory._
+
+class GiftHistorySpec extends AnyFlatSpec {
+
+  private val jsonStringGH: JsonString = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
+  private val giftHistory: Vector[GiftPair] = Vector(GiftPair("GeoHar", "JohLen"))
+
+  "A GiftHistory" should "add a new year" in {
+    assert(giftHistoryAddYear("NewBee")(giftHistory) == Vector(GiftPair("GeoHar", "JohLen"), GiftPair("NewBee", "NewBee")))
+  }
+
+  it should "return an updated giftHistory" in {
+    assert(giftHistoryUpdateGiftHistory(0)(GiftPair("me", "you"))(giftHistory) == Vector(GiftPair("me", "you")))
+  }
+
+  it should "convert from JSON" in {
+    val ghJson: Either[Error, Vector[GiftPair]] = giftHistoryJsonStringToGiftHistory(jsonStringGH)
+    assert(ghJson == Right(giftHistory))
+  }
+
+  it should "convert to JSON" in {
+    val ghJson: JsonString = giftHistoryToJsonString(giftHistory)
+    assert(ghJson == jsonStringGH)
+  }
+}
