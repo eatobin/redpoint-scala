e3c61c09c812c729f9a86999c9df0b24261376cc
Eric Tobin <eatobin@gmail.com>
2021-03-19 19:59:27 -0500

    curry

--- src/test/scala/redpoint/PlayerSpec.scala
+++ src/test/scala/redpoint/PlayerSpec.scala
@@ -9,7 +9,7 @@ class PlayerSpec extends AnyFlatSpec {
   private val player: Player = Player("Paul McCartney", Vector(GiftPair("GeoHar", "JohLen")))
 
   "A Player" should "return an updated giftHistory" in {
-    assert(Player.updateGiftHistory(Vector(GiftPair("nope", "yup")), player) ==
+    assert(Player.updateGiftHistory(Vector(GiftPair("nope", "yup")))(player) ==
       Player("Paul McCartney", Vector(GiftPair("nope", "yup"))))
   }
 
