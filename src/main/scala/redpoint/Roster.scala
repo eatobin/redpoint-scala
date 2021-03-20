8d2184b528f1b9efe6d5ac64fed11fa783d376c7
Eric Tobin <eatobin@gmail.com>
2021-03-20 17:13:40 -0500

    roster tests

--- src/main/scala/redpoint/Roster.scala
+++ src/main/scala/redpoint/Roster.scala
@@ -1,22 +1,22 @@
-//package redpoint
-//
-//import io.circe.Error
-//import io.circe.generic.auto._
-//import io.circe.parser._
-//
-//case class Roster(rosterName: String, rosterYear: Int, players: Map[String, Player])
-//
-//object Roster {
-//  def rosterJsonStringToRoster(s: Either[ErrorString, JsonString]): Either[ErrorString, Roster] = {
-//    s match {
-//      case Right(rFile) =>
-//        val res: Either[Error, Roster] = decode[Roster](rFile)
-//        res match {
-//          case Right(rDecode) => Right(rDecode)
-//          case Left(lDecode) => Left(lDecode.toString)
-//        }
-//      case Left(lFile) =>
-//        Left(lFile)
-//    }
-//  }
-//}
+package redpoint
+
+import io.circe.Error
+import io.circe.generic.auto._
+import io.circe.parser._
+
+case class Roster(rosterName: String, rosterYear: Int, players: Map[String, Player])
+
+object Roster {
+  def jsonStringToRoster(s: Either[ErrorString, JsonString]): Either[ErrorString, Roster] = {
+    s match {
+      case Right(rFile) =>
+        val res: Either[Error, Roster] = decode[Roster](rFile)
+        res match {
+          case Right(rDecode) => Right(rDecode)
+          case Left(lDecode) => Left(lDecode.toString)
+        }
+      case Left(lFile) =>
+        Left(lFile)
+    }
+  }
+}
