2686aa41fcf00f7b12c992e5bc7454e9a4065ea9
Eric Tobin <eatobin@gmail.com>
2021-03-20 17:20:15 -0500

    hat tests

--- src/main/scala/redpoint/Hats.scala
+++ src/main/scala/redpoint/Hats.scala
@@ -1,15 +1,15 @@
-//package redpoint
-//
-//object Hats {
-//  def makeHat(players: Map[String, Player]): Set[String] =
-//    players.keySet
-//
-//  def removePuck(hat: Set[String], playerKey: String): Set[String] =
-//    hat - playerKey
-//
-//  def discardGivee(discards: Set[String], givee: String): Set[String] =
-//    discards + givee
-//
-//  def returnDiscards(geHat: Set[String], discards: Set[String]): Set[String] =
-//    geHat ++ discards
-//}
+package redpoint
+
+object Hats {
+  def makeHat(players: Map[String, Player]): Set[String] =
+    players.keySet
+
+  def removePuck(playerKey: String)(hat: Set[String]): Set[String] =
+    hat - playerKey
+
+  def discardGivee(givee: String)(discards: Set[String]): Set[String] =
+    discards + givee
+
+  def returnDiscards(discards: Set[String])(geHat: Set[String]): Set[String] =
+    geHat ++ discards
+}
