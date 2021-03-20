362b237a6a9a4025728126eed9ceebf1a99c7952
Eric Tobin <eatobin@gmail.com>
2021-03-20 17:46:19 -0500

    rules tests

--- src/main/scala/redpoint/Players.scala
+++ src/main/scala/redpoint/Players.scala
@@ -21,11 +21,11 @@ object Players {
     nplrs
   }
 
-  def getGivee(giver: String)(giftYear: Int)(players: Map[String, Player]): String =
-    players(giver).giftHistory(giftYear).givee
+  def getGivee(selfKey: String)(giftYear: Int)(players: Map[String, Player]): String =
+    players(selfKey).giftHistory(giftYear).givee
 
-  def getGiver(givee: String)(giftYear: Int)(players: Map[String, Player]): String =
-    players(givee).giftHistory(giftYear).giver
+  def getGiver(selfKey: String)(giftYear: Int)(players: Map[String, Player]): String =
+    players(selfKey).giftHistory(giftYear).giver
 
   private def setGiftPair(playerKey: String)(giftYear: Int)(giftPair: GiftPair)(players: Map[String, Player]): Map[String, Player] = {
     val ngh = GiftHistory.updateGiftHistory(giftYear)(giftPair)(players(playerKey).giftHistory)
@@ -33,14 +33,14 @@ object Players {
     updatePlayer(playerKey)(nplr)(players)
   }
 
-  def updateGivee(giver: String)(giftYear: Int)(givee: String)(players: Map[String, Player]): Map[String, Player] = {
-    val ngp = GiftPair.updateGivee(givee)(players(giver).giftHistory(giftYear))
-    setGiftPair(giver)(giftYear)(ngp)(players)
+  def updateGivee(selfKey: String)(giftYear: Int)(givee: String)(players: Map[String, Player]): Map[String, Player] = {
+    val ngp = GiftPair.updateGivee(givee)(players(selfKey).giftHistory(giftYear))
+    setGiftPair(selfKey)(giftYear)(ngp)(players)
   }
 
-  def updateGiver(givee: String)(giftYear: Int)(giver: String)(players: Map[String, Player]): Map[String, Player] = {
-    val ngp = GiftPair.updateGiver(giver)(players(givee).giftHistory(giftYear))
-    setGiftPair(givee)(giftYear)(ngp)(players)
+  def updateGiver(selfKey: String)(giftYear: Int)(giver: String)(players: Map[String, Player]): Map[String, Player] = {
+    val ngp = GiftPair.updateGiver(giver)(players(selfKey).giftHistory(giftYear))
+    setGiftPair(selfKey)(giftYear)(ngp)(players)
   }
 
   def jsonStringToPlayers(plrsString: String): Either[Error, Map[String, Player]] =
--- src/main/scala/redpoint/Rules.scala
+++ src/main/scala/redpoint/Rules.scala
@@ -1,20 +1,18 @@
-//package redpoint
-//
-//import redpoint.Players.playersGetGivee
-//
-//object Rules {
-//  def giveeNotSelf(selfKey: String, givee: String): Boolean =
-//    selfKey != givee
-//
-//  def giveeNotRecip(selfKey: String, givee: String, giftYear: Int, players: Map[String, Player]): Boolean = {
-//    val recip = playersGetGivee(players, givee, giftYear)
-//    selfKey != recip
-//  }
-//
-//  def giveeNotRepeat(selfKey: String, givee: String, giftYear: Int, players: Map[String, Player]): Boolean = {
-//    val past = (giftYear - 1).to(giftYear - 3).by(-1).toVector.filterNot(y => y < 0)
-//    val geY = playersGetGivee(players, selfKey, _: Int)
-//    val geInYrs = past.map(gy => geY(gy))
-//    !geInYrs.contains(givee)
-//  }
-//}
+package redpoint
+
+object Rules {
+  def giveeNotSelf(selfKey: String, givee: String): Boolean =
+    selfKey != givee
+
+  def giveeNotRecip(selfKey: String, givee: String, giftYear: Int, players: Map[String, Player]): Boolean = {
+    val recip = Players.getGivee(givee)(giftYear)(players)
+    selfKey != recip
+  }
+
+  def giveeNotRepeat(selfKey: String, givee: String, giftYear: Int, players: Map[String, Player]): Boolean = {
+    val past = (giftYear - 1).to(giftYear - 3).by(-1).toVector.filterNot(y => y < 0)
+    val geY = Players.getGivee(selfKey)(_: Int)(players)
+    val geInYrs = past.map(gy => geY(gy))
+    !geInYrs.contains(givee)
+  }
+}
