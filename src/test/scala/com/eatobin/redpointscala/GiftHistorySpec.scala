6f989f68863df0e6460342725726d8b0ac4e3fa6
Eric Tobin <eatobin@gmail.com>
2022-11-06 21:20:09 -0600

    haskell-like

--- src/test/scala/com/eatobin/redpointscala/GiftHistorySpec.scala
+++ src/test/scala/com/eatobin/redpointscala/GiftHistorySpec.scala
@@ -1,29 +1,30 @@
 package com.eatobin.redpointscala
 
+import com.eatobin.redpointscala.GiftHistory._
 import com.eatobin.redpointscala.GiftPair.JsonString
 import io.circe.Error
 import org.scalatest.flatspec.AnyFlatSpec
 
 class GiftHistorySpec extends AnyFlatSpec {
 
-  private val jsonStringGH: JsonString = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
-  private val giftHistory: Vector[GiftPair] = Vector(GiftPair("GeoHar", "JohLen"))
+  private val jsonString: JsonString = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
+  private val giftHistory: GiftHistory = Vector(GiftPair("GeoHar", "JohLen"))
 
   "A GiftHistory" should "add a new year" in {
-    assert(GiftHistory.giftHistoryAddYear("NewBee")(giftHistory) == Vector(GiftPair("GeoHar", "JohLen"), GiftPair("NewBee", "NewBee")))
+    assert(giftHistoryAddYear("NewBee")(giftHistory) == Vector(GiftPair("GeoHar", "JohLen"), GiftPair("NewBee", "NewBee")))
   }
 
   it should "return an updated giftHistory" in {
-    assert(GiftHistory.giftHistoryUpdateGiftHistory(0)(GiftPair("me", "you"))(giftHistory) == Vector(GiftPair("me", "you")))
+    assert(giftHistoryUpdateGiftHistory(0)(GiftPair("me", "you"))(giftHistory) == Vector(GiftPair("me", "you")))
   }
 
   it should "convert from JSON" in {
-    val ghJson: Either[Error, Vector[GiftPair]] = GiftHistory.giftHistoryJsonStringToGiftHistory(jsonStringGH)
+    val ghJson: Either[Error, Vector[GiftPair]] = giftHistoryJsonStringToGiftHistory(jsonString)
     assert(ghJson == Right(giftHistory))
   }
 
   it should "convert to JSON" in {
-    val ghJson: JsonString = GiftHistory.giftHistoryGiftHistoryToJsonString(giftHistory)
-    assert(ghJson == jsonStringGH)
+    val ghJson: JsonString = giftHistoryGiftHistoryToJsonString(giftHistory)
+    assert(ghJson == jsonString)
   }
 }
