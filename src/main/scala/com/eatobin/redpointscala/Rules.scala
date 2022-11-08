4af4d68875421da6f8951e1be054e301d0654ee4
Eric Tobin <eatobin@gmail.com>
2022-11-08 17:38:45 -0600

    types

--- src/main/scala/com/eatobin/redpointscala/Rules.scala
+++ src/main/scala/com/eatobin/redpointscala/Rules.scala
@@ -1,20 +1,22 @@
 package com.eatobin.redpointscala
 
-import com.eatobin.redpointscala.Players.playersGetMyGivee
+import com.eatobin.redpointscala.GiftHistory.GiftYear
+import com.eatobin.redpointscala.GiftPair.{Givee, PlayerKey}
+import com.eatobin.redpointscala.Players.{Players, playersGetMyGivee}
 
 object Rules {
-  def rulesGiveeNotSelf(selfKey: String, givee: String): Boolean =
+  def rulesGiveeNotSelf(selfKey: PlayerKey, givee: Givee): Boolean =
     selfKey != givee
 
-  def rulesGiveeNotRecip(selfKey: String, givee: String, giftYear: Int, players: Map[String, Player]): Boolean = {
+  def rulesGiveeNotRecip(selfKey: PlayerKey, givee: Givee, giftYear: GiftYear, players: Players): Boolean = {
     val recip = playersGetMyGivee(givee)(giftYear)(players)
     selfKey != recip
   }
 
-  def rulesGiveeNotRepeat(selfKey: String, givee: String, giftYear: Int, players: Map[String, Player]): Boolean = {
-    val past: Vector[Int] = (giftYear - 1).to(giftYear - 3).by(-1).toVector.filterNot(y => y < 0)
-    val giveeInYear: Int => String = (giftYear: Int) => playersGetMyGivee(selfKey)(giftYear)(players)
-    val giveesInYears: Vector[String] = past.map(gy => giveeInYear(gy))
+  def rulesGiveeNotRepeat(selfKey: PlayerKey, givee: Givee, giftYear: GiftYear, players: Players): Boolean = {
+    val past: Vector[GiftYear] = (giftYear - 1).to(giftYear - 3).by(-1).toVector.filterNot(y => y < 0)
+    val giveeInYear: GiftYear => Givee = (giftYear: GiftYear) => playersGetMyGivee(selfKey)(giftYear)(players)
+    val giveesInYears: Vector[Givee] = past.map(gy => giveeInYear(gy))
     !giveesInYears.contains(givee)
   }
 }
