d7be9d7eb803473f4839fdb6af51c06e851b699c
Eric Tobin <eatobin@gmail.com>
2022-11-14 20:06:22 -0600

    reset state so that it tests

--- src/main/scala/com/eatobin/redpointscala/Main.scala
+++ src/main/scala/com/eatobin/redpointscala/Main.scala
@@ -5,23 +5,23 @@ import com.eatobin.redpointscala.Rules.{rulesGiveeNotRecip, rulesGiveeNotRepeat,
 
 object Main {
   def main(args: Array[String]): Unit = {
-    helpersRosterOrQuit(filePath)
-    while (helpersPrintAndAsk(aRosterName)(aRosterYear).toLowerCase != "q") {
-      helpersStartNewYear()
-      while (aMaybeGiver.isDefined) {
-        while (aMaybeGivee.isDefined) {
-          if (rulesGiveeNotSelf(aMaybeGiver.get, aMaybeGivee.get) &&
-            rulesGiveeNotRecip(aMaybeGiver.get, aMaybeGivee.get, aGiftYear, aPlayers) &&
-            rulesGiveeNotRepeat(aMaybeGiver.get, aMaybeGivee.get, aGiftYear, aPlayers)) {
-            helpersGiveeIsSuccess()
-          } else {
-            helpersGiveeIsFailure()
-          }
-        }
-        helpersSelectNewGiver()
-      }
-    }
-    println()
+//    helpersRosterOrQuit(filePath)
+//    while (helpersPrintAndAsk(aRosterName)(aRosterYear).toLowerCase != "q") {
+//      helpersStartNewYear()
+//      while (aMaybeGiver.isDefined) {
+//        while (aMaybeGivee.isDefined) {
+//          if (rulesGiveeNotSelf(aMaybeGiver.get, aMaybeGivee.get) &&
+//            rulesGiveeNotRecip(aMaybeGiver.get, aMaybeGivee.get, aGiftYear, aPlayers) &&
+//            rulesGiveeNotRepeat(aMaybeGiver.get, aMaybeGivee.get, aGiftYear, aPlayers)) {
+//            helpersGiveeIsSuccess()
+//          } else {
+//            helpersGiveeIsFailure()
+//          }
+//        }
+//        helpersSelectNewGiver()
+//      }
+//    }
+//    println()
     println("This was fun!")
     println("Talk about a position with Redpoint?")
     println("Please call: Eric Tobin 773-679-6617")
--- src/test/scala/com/eatobin/redpointscala/HelpersSpec.scala
+++ src/test/scala/com/eatobin/redpointscala/HelpersSpec.scala
@@ -1,27 +1,33 @@
 package com.eatobin.redpointscala
 
+import com.eatobin.redpointscala.GiftPair.JsonString
+import com.eatobin.redpointscala.Helpers._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class HelpersSpec extends AnyFlatSpec {
 
-  private val filePath: String = "src/test/resources/beatles.json"
-  private val badFilePath: String = "nope.json"
-  private val badJsonFile: String = "src/test/resources/bad-json.json"
-
-  private val rinSta: Player = Player("Ringo Starr", Vector(GiftPair("JohLen", "GeoHar")))
-  private val johLen: Player = Player("John Lennon", Vector(GiftPair("PauMcc", "RinSta")))
-  private val geoHar: Player = Player("George Harrison", Vector(GiftPair("RinSta", "PauMcc")))
-  private val pauMcc: Player = Player("Paul McCartney", Vector(GiftPair("GeoHar", "JohLen")))
-  private val players: Map[String, Player] =
-    Map("RinSta" -> rinSta, "JohLen" -> johLen, "GeoHar" -> geoHar, "PauMcc" -> pauMcc)
-
-  private val geoWhoops: Player = Player("George Harrison", Vector(GiftPair("GeoHar", "PauMcc")))
-  private val pauYikes: Player = Player("Paul McCartney", Vector(GiftPair("GeoHar", "PauMcc")))
-  private val playersWeird: Map[String, Player] =
-    Map("RinSta" -> rinSta, "JohLen" -> johLen, "GeoHar" -> geoWhoops, "PauMcc" -> pauYikes)
-
-  private val rinStaPlus: Player = Player("Ringo Starr", Vector(GiftPair("JohLen", "GeoHar"), GiftPair("RinSta", "RinSta")))
-  private val testHat: Set[String] = Set("RinSta")
+  private val filePath: String = "src/test/resources/beatles-state.json"
+  private val jsonString: JsonString = "{  \"rosterName\": \"The Beatles\",  \"rosterYear\": 2014,  \"players\": {    \"PauMcc\": {      \"playerName\": \"Paul McCartney\",      \"giftHistory\": [        {          \"givee\": \"GeoHar\",          \"giver\": \"JohLen\"        }      ]    },    \"GeoHar\": {      \"playerName\": \"George Harrison\",      \"giftHistory\": [        {          \"givee\": \"RinSta\",          \"giver\": \"PauMcc\"        }      ]    },    \"JohLen\": {      \"playerName\": \"John Lennon\",      \"giftHistory\": [        {          \"givee\": \"PauMcc\",          \"giver\": \"RinSta\"        }      ]    },    \"RinSta\": {      \"playerName\": \"Ringo Starr\",      \"giftHistory\": [        {          \"givee\": \"JohLen\",          \"giver\": \"GeoHar\"        }      ]    }  },  \"giftYear\": 0,  \"giveeHat\": null,  \"giverHat\": null,  \"maybeGivee\": null,  \"maybeGiver\": null,  \"discards\": null}"
+  //  private val badFilePath: String = "nope.json"
+  //  private val badJsonFile: String = "src/test/resources/bad-json.json"
+  //
+  //  private val rinSta: Player = Player("Ringo Starr", Vector(GiftPair("JohLen", "GeoHar")))
+  //  private val johLen: Player = Player("John Lennon", Vector(GiftPair("PauMcc", "RinSta")))
+  //  private val geoHar: Player = Player("George Harrison", Vector(GiftPair("RinSta", "PauMcc")))
+  //  private val pauMcc: Player = Player("Paul McCartney", Vector(GiftPair("GeoHar", "JohLen")))
+  //  private val players: Map[String, Player] =
+  //    Map("RinSta" -> rinSta, "JohLen" -> johLen, "GeoHar" -> geoHar, "PauMcc" -> pauMcc)
+  //
+  //  private val geoWhoops: Player = Player("George Harrison", Vector(GiftPair("GeoHar", "PauMcc")))
+  //  private val pauYikes: Player = Player("Paul McCartney", Vector(GiftPair("GeoHar", "PauMcc")))
+  //  private val playersWeird: Map[String, Player] =
+  //    Map("RinSta" -> rinSta, "JohLen" -> johLen, "GeoHar" -> geoWhoops, "PauMcc" -> pauYikes)
+  //
+  //  private val rinStaPlus: Player = Player("Ringo Starr", Vector(GiftPair("JohLen", "GeoHar"), GiftPair("RinSta", "RinSta")))
+  //  private val testHat: Set[String] = Set("RinSta")
+  "helpersReadFileIntoJsonString" should "construct a Right(RosterString)" in {
+    assert(helpersReadFileIntoJsonString(filePath) == Right(jsonString))
+  }
 
   //  "Redpoint" should "build a Roster" in {
   //    helpersRosterOrQuit(filePath)
