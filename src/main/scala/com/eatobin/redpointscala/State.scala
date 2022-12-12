6654b93bf65b7a319c77c0f5f16eea937995eaf5
Eric Tobin <eatobin@gmail.com>
2022-12-11 21:48:37 -0600

    hat.scala tests

--- src/main/scala/com/eatobin/redpointscala/State.scala
+++ src/main/scala/com/eatobin/redpointscala/State.scala
@@ -1,9 +1,10 @@
-//package com.eatobin.redpointscala
-//
-//import com.eatobin.redpointscala.GiftHistory.GiftYear
-//import com.eatobin.redpointscala.GiftPair.{Givee, Giver}
-//import com.eatobin.redpointscala.Players.Players
-//import com.eatobin.redpointscala.Roster.{RosterName, RosterYear}
-//
-//case class State(rosterName: RosterName, rosterYear: RosterYear, players: Players, giftYear: GiftYear, giveeHat: Hat, giverHat: Hat, maybeGivee: Option[Givee], maybeGiver: Option[Giver], discards: Hat)
+package com.eatobin.redpointscala
+
+import com.eatobin.redpointscala.GiftHistory.GiftYear
+import com.eatobin.redpointscala.GiftPair.{Givee, Giver}
+import com.eatobin.redpointscala.Hat.Hat
+import com.eatobin.redpointscala.Players.Players
+import com.eatobin.redpointscala.Roster.{RosterName, RosterYear}
+
+case class State(rosterName: RosterName, rosterYear: RosterYear, players: Players, giftYear: GiftYear, giveeHat: Hat, giverHat: Hat, maybeGivee: Option[Givee], maybeGiver: Option[Giver], discards: Hat)
 //  private val roster: Roster = Roster("The Beatles", 2014, players, 0, emptyHat, emptyHat, None, None, emptyHat)
