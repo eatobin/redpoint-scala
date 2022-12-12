4c14a1f524494abfc7f28b44b04693c335c69017
Eric Tobin <eatobin@gmail.com>
2022-12-11 21:25:21 -0600

    GiftHistory.scala tests

--- src/test/scala/com/eatobin/redpointscala/GiftHistorySpec.scala
+++ src/test/scala/com/eatobin/redpointscala/GiftHistorySpec.scala
@@ -1,30 +1,17 @@
-//package com.eatobin.redpointscala
-//
-//import com.eatobin.redpointscala.GiftHistory._
-//import com.eatobin.redpointscala.GiftPair.JsonString
-//import io.circe.Error
-//import org.scalatest.flatspec.AnyFlatSpec
-//
-//class GiftHistorySpec extends AnyFlatSpec {
-//
-//  private val jsonString: JsonString = "[{\"givee\":\"GeoHar\",\"giver\":\"JohLen\"}]"
-//  private val giftHistory: GiftHistory = Vector(GiftPair("GeoHar", "JohLen"))
-//
-//  "A GiftHistory" should "add a new year" in {
-//    assert(giftHistoryAddYear("NewBee")(giftHistory) == Vector(GiftPair("GeoHar", "JohLen"), GiftPair("NewBee", "NewBee")))
-//  }
-//
-//  it should "return an updated giftHistory" in {
-//    assert(giftHistoryUpdateGiftHistory(0)(GiftPair("me", "you"))(giftHistory) == Vector(GiftPair("me", "you")))
-//  }
-//
-//  it should "convert from JSON" in {
-//    val ghJson: Either[Error, Vector[GiftPair]] = giftHistoryJsonStringToGiftHistory(jsonString)
-//    assert(ghJson == Right(giftHistory))
-//  }
-//
-//  it should "convert to JSON" in {
-//    val ghJson: JsonString = giftHistoryGiftHistoryToJsonString(giftHistory)
-//    assert(ghJson == jsonString)
-//  }
-//}
+package com.eatobin.redpointscala
+
+import com.eatobin.redpointscala.GiftHistory._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class GiftHistorySpec extends AnyFlatSpec {
+
+  private val giftHistory: GiftHistory = Vector(GiftPair("GeoHar", "JohLen"))
+
+  "A GiftHistory" should "add a new year" in {
+    assert(giftHistoryAddYear("NewBee")(giftHistory) == Vector(GiftPair("GeoHar", "JohLen"), GiftPair("NewBee", "NewBee")))
+  }
+
+  it should "return an updated giftHistory" in {
+    assert(giftHistoryUpdateGiftHistory(0)(GiftPair("me", "you"))(giftHistory) == Vector(GiftPair("me", "you")))
+  }
+}
