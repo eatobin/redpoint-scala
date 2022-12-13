2f7168a1e8eb4b6f4e61acab4747047218ccd8d0
Eric Tobin <eatobin@gmail.com>
2022-12-12 22:03:41 -0700

    state progress

--- src/main/scala/com/eatobin/redpointscala/State.scala
+++ src/main/scala/com/eatobin/redpointscala/State.scala
@@ -3,51 +3,71 @@ package com.eatobin.redpointscala
 import com.eatobin.redpointscala.GiftHistory.GiftYear
 import com.eatobin.redpointscala.GiftPair.{Givee, Giver, PlayerKey}
 import com.eatobin.redpointscala.Hat.{Hat, hatMakeHat, hatRemovePuck, hatReturnDiscards}
-import com.eatobin.redpointscala.Players.{Players, playersAddYear}
+import com.eatobin.redpointscala.Players.{Players, playersAddYear, playersUpdateMyGivee, playersUpdateMyGiver}
 import com.eatobin.redpointscala.Roster.{RosterName, RosterYear}
 
 case class State(rosterName: RosterName, rosterYear: RosterYear, players: Players, giftYear: GiftYear, giveeHat: Hat, giverHat: Hat, maybeGivee: Option[Givee], maybeGiver: Option[Giver], discards: Hat)
 
-private def stateRandom(hat: Hat): PlayerKey = {
-  val n: Int = util.Random.nextInt(hat.size)
-  hat.iterator.drop(n).next()
-}
-
-def stateDrawPuck(hat: Hat): Option[PlayerKey] = {
-  if (hat.nonEmpty) {
-    Some(stateRandom(hat))
-  } else {
-    None
+object State {
+  private def stateRandom(hat: Hat): PlayerKey = {
+    val n: Int = util.Random.nextInt(hat.size)
+    hat.iterator.drop(n).next()
   }
-}
 
-def stateStartNewYear(state: State): State = {
-  val newState: State = State(
-    rosterName = state.rosterName,
-    rosterYear = state.rosterYear,
-    players = playersAddYear(state.players),
-    giftYear = state.giftYear + 1,
-    giveeHat = hatMakeHat(state.players),
-    giverHat = hatMakeHat(state.players),
-    maybeGivee = stateDrawPuck(hatMakeHat(state.players)),
-    maybeGiver = stateDrawPuck(hatMakeHat(state.players)),
-    discards = Set()
-  )
-  newState
-}
+  private def stateDrawPuck(hat: Hat): Option[PlayerKey] = {
+    if (hat.nonEmpty) {
+      Some(stateRandom(hat))
+    } else {
+      None
+    }
+  }
 
-def stateSelectNewGiver(state: State): State = {
-  val giver: Giver = state.maybeGiver.get
-  val newState: State = State(
-    rosterName = state.rosterName,
-    rosterYear = state.rosterYear,
-    players = state.players,
-    giftYear = state.giftYear,
-    giveeHat = hatReturnDiscards(state.discards, state.giveeHat),
-    giverHat = hatRemovePuck(giver, state.giverHat),
-    maybeGivee = stateDrawPuck(hatMakeHat(state.players)),
-    maybeGiver = stateDrawPuck(hatMakeHat(state.players)),
-    discards = Set()
-  )
-  newState
+  def stateStartNewYear(state: State): State = {
+    val newState: State = State(
+      rosterName = state.rosterName,
+      rosterYear = state.rosterYear,
+      players = playersAddYear(state.players),
+      giftYear = state.giftYear + 1,
+      giveeHat = hatMakeHat(state.players),
+      giverHat = hatMakeHat(state.players),
+      maybeGivee = stateDrawPuck(hatMakeHat(state.players)),
+      maybeGiver = stateDrawPuck(hatMakeHat(state.players)),
+      discards = Set()
+    )
+    newState
+  }
+
+  def stateSelectNewGiver(state: State): State = {
+    val giver: Giver = state.maybeGiver.get
+    val newState: State = State(
+      rosterName = state.rosterName,
+      rosterYear = state.rosterYear,
+      players = state.players,
+      giftYear = state.giftYear,
+      giveeHat = hatReturnDiscards(state.discards, state.giveeHat),
+      giverHat = hatRemovePuck(giver, state.giverHat),
+      maybeGivee = stateDrawPuck(hatMakeHat(state.players)),
+      maybeGiver = stateDrawPuck(hatMakeHat(state.players)),
+      discards = Set()
+    )
+    newState
+  }
+
+  def stateGiveeIsSuccess(state: State): State = {
+    val giver: Giver = state.maybeGiver.get
+    val givee: Givee = state.maybeGivee.get
+    val tempPlayers = playersUpdateMyGivee(giver)(state.giftYear)(givee)(state.players)
+    val newState: State = State(
+      rosterName = state.rosterName,
+      rosterYear = state.rosterYear,
+      players = playersUpdateMyGiver(givee)(state.giftYear)(giver)(tempPlayers),
+      giftYear = state.giftYear,
+      giveeHat = hatRemovePuck(givee, state.giveeHat),
+      giverHat = state.giverHat,
+      maybeGivee = None,
+      maybeGiver = state.maybeGiver,
+      discards = state.discards
+    )
+    newState
+  }
 }
