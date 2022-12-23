4a63df22527e96e52b35a5161d6df0b38d781189
Eric Tobin <eatobin@gmail.com>
2022-12-23 12:19:28 -0700

    main progress

--- src/main/scala/com/eatobin/redpointscala/Main.scala
+++ src/main/scala/com/eatobin/redpointscala/Main.scala
@@ -7,31 +7,40 @@ object Main {
   private val joeQue: Player = Player("Joel Quenneville", Vector(GiftPair("TroBro", "AndLad")))
   private val adaBur: Player = Player("Adam Burish", Vector(GiftPair("DunKei", "JonToe")))
   private val andLad: Player = Player("Andrew Ladd", Vector(GiftPair("JoeQue", "KriVer")))
+
+  private val antNie: Player = Player("Antti Niemi", Vector(GiftPair("JonToe", "MarHos")))
+  private val breSea: Player = Player("Brent Seabrook", Vector(GiftPair("KriVer", "NikHja")))
+  private val bryBic: Player = Player("Bryan Bickell", Vector(GiftPair("MarHos", "PatKan")))
+  private val andLad: Player = Player("Andrew Ladd", Vector(GiftPair("JoeQue", "KriVer")))
+  private val andLad: Player = Player("Andrew Ladd", Vector(GiftPair("JoeQue", "KriVer")))
+  private val andLad: Player = Player("Andrew Ladd", Vector(GiftPair("JoeQue", "KriVer")))
+  private val andLad: Player = Player("Andrew Ladd", Vector(GiftPair("JoeQue", "KriVer")))
+
   // TODO keep adding players from blackhawks.json!!
   private val players: Map[String, Player] =
     Map("TroBro" -> troBro, "JoeQue" -> joeQue, "AdaBur" -> adaBur, "AndLad" -> andLad)
 
-  def main(args: Array[String]): Unit = {
-    val x = rinSta
-    while (helpersPrintAndAsk(aRosterName)(aRosterYear).toLowerCase != "q") {
-      helpersStartNewYear()
-      while (aMaybeGiver.isDefined) {
-        while (aMaybeGivee.isDefined) {
-          if (rulesGiveeNotSelf(aMaybeGiver.get, aMaybeGivee.get) &&
-            rulesGiveeNotRecip(aMaybeGiver.get, aMaybeGivee.get, aGiftYear, aPlayers) &&
-            rulesGiveeNotRepeat(aMaybeGiver.get, aMaybeGivee.get, aGiftYear, aPlayers)) {
-            helpersGiveeIsSuccess()
-          } else {
-            helpersGiveeIsFailure()
-          }
-        }
-        helpersSelectNewGiver()
-      }
-    }
-    println()
-    println("This was fun!")
-    println("Talk about a position with Redpoint?")
-    println("Please call: Eric Tobin 773-679-6617")
-    println()
-  }
+//  def main(args: Array[String]): Unit = {
+//    val x = rinSta
+//    while (helpersPrintAndAsk(aRosterName)(aRosterYear).toLowerCase != "q") {
+//      helpersStartNewYear()
+//      while (aMaybeGiver.isDefined) {
+//        while (aMaybeGivee.isDefined) {
+//          if (rulesGiveeNotSelf(aMaybeGiver.get, aMaybeGivee.get) &&
+//            rulesGiveeNotRecip(aMaybeGiver.get, aMaybeGivee.get, aGiftYear, aPlayers) &&
+//            rulesGiveeNotRepeat(aMaybeGiver.get, aMaybeGivee.get, aGiftYear, aPlayers)) {
+//            helpersGiveeIsSuccess()
+//          } else {
+//            helpersGiveeIsFailure()
+//          }
+//        }
+//        helpersSelectNewGiver()
+//      }
+//    }
+//    println()
+//    println("This was fun!")
+//    println("Talk about a position with Redpoint?")
+//    println("Please call: Eric Tobin 773-679-6617")
+//    println()
+//  }
 }
