bfb3b09292b9d6249ef33a35db35d31f19a2c9f9
Eric Tobin <eatobin@gmail.com>
2023-01-29 18:36:35 -0700

    print state

--- src/main/scala/com/eatobin/redpointscala/State.scala
+++ src/main/scala/com/eatobin/redpointscala/State.scala
@@ -150,7 +150,8 @@ object State {
   }
 
   def statePrintResults(state: State): State = {
-    print(stateErrors(state))
+    println(stateErrors(state))
+    println(state)
     println()
     println("%s - Year %d Gifts:".format(state.rosterName, state.rosterYear + state.giftYear))
     println()
