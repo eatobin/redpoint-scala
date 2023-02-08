0458dfe9adf5759553f0b6e4e8cbb26745c780b9
Eric Tobin <eatobin@gmail.com>
2023-02-08 10:49:06 -0700

    susSmi

--- src/test/scala/com/eatobin/redpointscala/RulesSpec.scala
+++ src/test/scala/com/eatobin/redpointscala/RulesSpec.scala
@@ -6,14 +6,14 @@ import scala.collection.immutable.SortedMap
 
 class RulesSpec extends AnyFlatSpec {
 
-  private val rinSta: Player = Player("Ringo Starr", Vector(GiftPair(giver = "KarLav", givee = "JohLen")))
+  private val rinSta: Player = Player("Ringo Starr", Vector(GiftPair(giver = "SusSmi", givee = "JohLen")))
   private val johLen: Player = Player("John Lennon", Vector(GiftPair(giver = "RinSta", givee = "GeoHar")))
   private val geoHar: Player = Player("George Harrison", Vector(GiftPair(giver = "JohLen", givee = "PauMcc")))
   private val pauMcc: Player = Player("Paul McCartney", Vector(GiftPair(giver = "GeoHar", givee = "EriTob")))
-  private val eriTob: Player = Player("Eric Tobin", Vector(GiftPair(giver = "PauMcc", givee = "KarLav")))
-  private val karLav: Player = Player("Karen Lavengood", Vector(GiftPair(giver = "EriTob", givee = "RinSta")))
+  private val eriTob: Player = Player("Eric Tobin", Vector(GiftPair(giver = "PauMcc", givee = "SusSmi")))
+  private val susSmi: Player = Player("Susan Smith", Vector(GiftPair(giver = "EriTob", givee = "RinSta")))
   private val beatlesPlusPM: SortedMap[String, Player] =
-    SortedMap("RinSta" -> rinSta, "JohLen" -> johLen, "GeoHar" -> geoHar, "PauMcc" -> pauMcc, "EriTob" -> eriTob, "KarLav" -> karLav)
+    SortedMap("RinSta" -> rinSta, "JohLen" -> johLen, "GeoHar" -> geoHar, "PauMcc" -> pauMcc, "EriTob" -> eriTob, "SusSmi" -> susSmi)
 
   private var beatlesPlus4 = Players.playersAddYear(beatlesPlusPM)
   beatlesPlus4 = Players.playersUpdateMyGivee("RinSta")("GeoHar")(1)(beatlesPlus4)
@@ -22,7 +22,7 @@ class RulesSpec extends AnyFlatSpec {
   beatlesPlus4 = Players.playersAddYear(beatlesPlus4)
   beatlesPlus4 = Players.playersUpdateMyGivee("RinSta")("EriTob")(3)(beatlesPlus4)
   beatlesPlus4 = Players.playersAddYear(beatlesPlus4)
-  beatlesPlus4 = Players.playersUpdateMyGivee("RinSta")("KarLav")(4)(beatlesPlus4)
+  beatlesPlus4 = Players.playersUpdateMyGivee("RinSta")("SusSmi")(4)(beatlesPlus4)
 
   "A Player" should "not give to itself" in {
     assert(Rules.rulesGiveeNotSelf("RinSta", "GeoHar"))
@@ -31,17 +31,17 @@ class RulesSpec extends AnyFlatSpec {
 
   it should "not give to it's recip" in {
     assert(Rules.rulesGiveeNotRecip("RinSta", "JohLen", 0, beatlesPlusPM))
-    assert(!Rules.rulesGiveeNotRecip("RinSta", "KarLav", 0, beatlesPlusPM))
+    assert(!Rules.rulesGiveeNotRecip("RinSta", "SusSmi", 0, beatlesPlusPM))
   }
 
   it should "not repeat for four years" in {
     assert(!Rules.rulesGiveeNotRepeat("RinSta", "JohLen", 2, beatlesPlus4))
     assert(!Rules.rulesGiveeNotRepeat("RinSta", "GeoHar", 2, beatlesPlus4))
-    assert(Rules.rulesGiveeNotRepeat("RinSta", "KarLav", 2, beatlesPlus4))
+    assert(Rules.rulesGiveeNotRepeat("RinSta", "SusSmi", 2, beatlesPlus4))
     assert(Rules.rulesGiveeNotRepeat("RinSta", "JohLen", 5, beatlesPlus4))
 //    assert(Rules.rulesGiveeNotRepeat("RinSta", "GeoHar", 5, beatlesPlus4))
 //    assert(!Rules.rulesGiveeNotRepeat("RinSta", "PauMcc", 5, beatlesPlus4))
 //    assert(!Rules.rulesGiveeNotRepeat("RinSta", "EriTob", 5, beatlesPlus4))
-//    assert(!Rules.rulesGiveeNotRepeat("RinSta", "KarLav", 5, beatlesPlus4))
+//    assert(!Rules.rulesGiveeNotRepeat("RinSta", "SusSmi", 5, beatlesPlus4))
   }
 }
